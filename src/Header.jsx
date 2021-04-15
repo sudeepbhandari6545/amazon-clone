a799bf7cf3beb3db3b5e08565e5b930b75ab9d97
sudeepbhandari6545 <sudeepbhandari6545@gmail.com>
2021-04-15 07:37:30 +0545

    first commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 import './Header.css';
 
 const Header = () => {
@@ -10,24 +12,33 @@ const Header = () => {
       />
       {/* Header search */}
       <div className="header_search">
-        <input type="text" placeholder="it's working" />
+        <input
+          className="header_input"
+          type="text"
+          placeholder="it's working"
+        />
+        <SearchIcon className="header_searchicon" />
       </div>
       {/* header navs */}
       <div className="header_nav">
         {/* nav first */}
-        <div className="header_nav">
+        <div className="header_nav_option">
           <span className="header_nav_one">Hello guest</span>
           <span className="header_nav_two">Sign In</span>
         </div>
         {/* nav second */}
-        <div className="header_nav">
+        <div className="header_nav_option">
           <span className="header_nav_one">Return</span>
           <span className="header_nav_two">& Order</span>
         </div>
         {/* nav third */}
-        <div className="header_nav">
+        <div className="header_nav_option">
           <span className="header_nav_one">your</span>
           <span className="header_nav_two">Prime</span>
+        </div>
+        <div className="header_basket">
+          <ShoppingBasketIcon />
+          <span className="header_nav_two header_basket_count">0</span>
         </div>
       </div>
     </div>
